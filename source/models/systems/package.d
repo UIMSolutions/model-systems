202d4f95a4bef8570ec94a69c40bce99cd801355
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-12 11:26:51 +0200

    Import attributes

--- source/models/systems/package.d
+++ source/models/systems/package.d
@@ -15,5 +15,6 @@ public import uim.oop;
 public import uim.entities;
 
 // model-system packages
+public import models.systems.attributes;
 public import models.systems.classes;
 public import models.systems.entities;
