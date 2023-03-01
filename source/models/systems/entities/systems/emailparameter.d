51c4aa7ea584d082a69cfe68baa8efe936ae1f15
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-01 13:05:04 +0100

    Adding license info

--- source/models/systems/entities/systems/emailparameter.d
+++ source/models/systems/entities/systems/emailparameter.d
@@ -1,3 +1,8 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
 module models.systems.entities.systems.emailparameter;
 
 @safe:
@@ -28,7 +33,7 @@ class DSystemEmailParameterEntity : DEntity {
 mixin(EntityCalls!("SystemEmailParameterEntity"));
 
 version(test_model_systsms) { unittest {
-    assert(APLFeedback);
+    
     assert(SystemEmailParameterEntity);
   
   auto entity = SystemEmailParameterEntity;
