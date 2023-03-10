9f04478d3615f3e6625e412b19f4df13eda9efed
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-03-10 15:17:54 +0100

    Bugfixing

--- source/models/systems/entities/systems/users/groups/userlist.d
+++ source/models/systems/entities/systems/users/groups/userlist.d
@@ -11,7 +11,7 @@ import models.systems;
 class DSystemUserGroupUserListEntity : DEntity {
   mixin(EntityThis!("SystemUserGroupUserListEntity"));
 
-  override void initialize(DConfigurationValue configSettings = null) {
+  override void initialize(Json configSettings = Json(null)) {
     super.initialize(configSettings);
 
     this
--- source/models/systems/entities/systems/users/user.d
+++ source/models/systems/entities/systems/users/user.d
@@ -12,7 +12,7 @@ import models.systems;
 class DSystemUserEntity : DEntity {
   mixin(EntityThis!("SystemUserEntity"));
 
-  override void initialize(DConfigurationValue configSettings = null) {
+  override void initialize(Json configSettings = Json(null)) {
     super.initialize(configSettings);
 
     this
