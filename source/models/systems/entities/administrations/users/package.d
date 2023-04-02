8ff6d45c2c92f12a863ef7d7e3aca856aae3b31b
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:44:12 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/users/package.d
@@ -0,0 +1,16 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.systems.users;
+
+@safe:
+import models.systems;
+
+// Main entity
+public import models.systems.entities.systems.users.user;
+
+// Subpackage
+public import models.systems.entities.systems.users.groups;
+
