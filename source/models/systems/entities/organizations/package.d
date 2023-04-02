47810846493d8f6fc061e3de19309cfc55c527ae
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 14:20:35 +0200

    Merging models

--- source/models/systems/entities/organizations/package.d
+++ source/models/systems/entities/organizations/package.d
@@ -2,3 +2,6 @@ module models.systems.entities.organizations;
 
 @safe:
 import models.systems;
+
+public import models.systems.entities.organizations.templatemessagesystem;
+public import models.systems.entities.organizations.templatesystem;
