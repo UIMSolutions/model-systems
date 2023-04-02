5c52b4d401fb7f600a5a4d38620497476250952e
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:43:46 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/currencies/package.d
@@ -0,0 +1,13 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.currencies;
+
+@safe:
+import models.systems;
+
+public import models.systems.entities.currencies.currency;
+public import models.systems.entities.currencies.isocode;
+public import models.systems.entities.currencies.revaluationaccount;
