4fcb95c4c2c67c52c1bd01d67c7e23b0e683a842
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:42:39 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/configurators/definitions/groups/tables/table.d
@@ -0,0 +1,39 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.configurators.definitions.groups.tables.table;
+
+@safe:
+import models.systems;
+
+// 
+class DConfiguratorDefinitionGroupTableEntity : DEntity {
+  mixin(EntityThis!("ConfiguratorDefinitionGroupTableEntity"));
+
+  override void initialize(Json configSettings = Json(null)) {
+    super.initialize(configSettings);
+
+    this
+      .addValues([
+        "definitionGroupId": UUIDAttribute, // 
+        "dataSourceNumber": StringAttribute, // 
+        "relatedTableName": StringAttribute, // 
+        "sysRelation": StringAttribute, // 
+        "tableName": StringAttribute, // 
+        "relationship_ConfiguratorDefinitionGroupEntityRelationshipId": UUIDAttribute, // 
+        "backingTable_ConfTableSelection_BRRelationshipId": UUIDAttribute, // 
+        "relationship_PrimaryCompanyContextRelationshipId": UUIDAttribute, // 
+      ])
+      .registerPath("system_configuratordefinitiongrouptables");
+  }
+}
+mixin(EntityCalls!("ConfiguratorDefinitionGroupTableEntity"));
+
+version(test_model_systsms) { unittest {
+    assert(ConfiguratorDefinitionGroupTableEntity);
+
+    auto entity = ConfiguratorDefinitionGroupTableEntity;
+  }
+}
---
+++ source/models/systems/entities/administrations/configurators/layouts/group.d
@@ -0,0 +1,45 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.configurators.layouts.group;
+
+@safe:
+import models.systems;
+
+class DConfiguratorLayoutGroupEntity : DEntity {
+  mixin(EntityThis!("ConfiguratorLayoutGroupEntity"));
+
+  override void initialize(Json configSettings = Json(null)) {
+    super.initialize(configSettings);
+
+    this
+      .addValues([
+        "layoutGroupId": UUIDAttribute, //
+        
+        "layoutType": StringAttribute, //
+        "definitionGroupId": UUIDAttribute, //
+        "fileStructure": StringAttribute, //
+        "delimitedBy": StringAttribute, //
+        "delimiterCharacter": StringAttribute, //
+        "textQualifier": StringAttribute, //
+        "export": StringAttribute, //
+        "import": StringAttribute, //
+        "registerType": StringAttribute, //
+        "relationship_ConfiguratorDefinitionGroupEntityRelationshipId": UUIDAttribute, //
+        "backingTable_ConfLayoutGroups_BRRelationshipId": UUIDAttribute, //
+        "relationship_PrimaryCompanyContextRelationshipId": UUIDAttribute, //  
+    ])
+      .registerPath("system_configurators.layouts.groups");
+  }
+}
+mixin(EntityCalls!("ConfiguratorLayoutGroupEntity"));
+
+version(test_model_systsms) { unittest {
+    assert(ConfiguratorLayoutGroupEntity);
+
+    auto entity = ConfiguratorLayoutGroupEntity;
+    // TODO more tests
+  }
+}
