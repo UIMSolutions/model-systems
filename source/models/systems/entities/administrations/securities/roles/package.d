9c01a8e2711101eeb6211db090be247ca674084d
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:44:07 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/securities/roles/package.d
@@ -0,0 +1,17 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.systems.securities.roles;
+
+@safe:
+import models.systems;
+
+public import models.systems.entities.systems.securities.roles.users;
+
+public import models.systems.entities.systems.securities.roles.role;
+
+public import models.systems.entities.systems.securities.roles.customization;
+public import models.systems.entities.systems.securities.roles.duty;
+public import models.systems.entities.systems.securities.roles.subrole;
