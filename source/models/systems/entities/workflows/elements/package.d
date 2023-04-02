9b5290f96a6ad59a2b33a11b2eeb11b7f90aa411
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:53:33 +0200

    Merging models

---
+++ source/models/systems/entities/workflows/elements/package.d
@@ -0,0 +1,18 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.workflows.entities.elements;
+
+// Main entity
+public import models.workflows.entities.elements.element;
+
+// Additional entities (modules)
+public import models.workflows.entities.elements.action;
+public import models.workflows.entities.elements.link;
+public import models.workflows.entities.elements.outcome_message;
+
+// Additional entities (packages)
+public import models.workflows.entities.elements.notifications;
+
