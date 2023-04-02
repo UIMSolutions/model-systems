ff5965784c166c9b49e3caa9f4a9dd5eb44371e3
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:43:01 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/documents/package.d
@@ -0,0 +1,16 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.documents;
+
+// Subpackage
+public import models.systems.entities.documents.routings;
+
+// Modules
+public import models.systems.entities.documents.enabledtable;
+public import models.systems.entities.documents.filetype;
+public import models.systems.entities.documents.parameter;
+public import models.systems.entities.documents.reference;
+public import models.systems.entities.documents.type;
