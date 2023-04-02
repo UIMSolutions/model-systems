454ce3d71687a0380c5ae5afa26b3e2ce55233f9
UIM GIT-Master <ui.manufaktur@gmail.com>
2023-04-02 13:42:46 +0200

    Merging models

---
+++ source/models/systems/entities/administrations/datamanagements/package.d
@@ -0,0 +1,15 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/
+module models.systems.entities.datamanagements;
+
+@safe:
+import models.systems;
+
+public import models.systems.entities.datamanagements.datamanagement;
+public import models.systems.entities.datamanagements.definitiongroups;
+public import models.systems.entities.datamanagements.executionjobs;
+public import models.systems.entities.datamanagements.targetmap;
+public import models.systems.entities.datamanagements.templates;
